69f1483534bcf02853cfa35b1322209b284a3d56
arturMucci <artur.mucci@gmail.com>
2022-11-03 20:32:29 -0300

    project-trybe-tunes - milestone01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
-// Abrindo PR.
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route path="/" component={ Login } exact />
+        <Route path="/search" component={ Search } exact />
+        <Route path="/album/:id" component={ Album } exact />
+        <Route path="/favorites" component={ Favorites } exact />
+        <Route path="/profile" component={ Profile } exact />
+        <Route path="/profile/edit" component={ ProfileEdit } exact />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
